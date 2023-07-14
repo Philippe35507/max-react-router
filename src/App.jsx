c9f5fb6c17cb8775f6e2e703fde28af499ba89ee
Polybe <55062877+Philippe35507@users.noreply.github.com>
2023-07-14 21:08:21 +0100

    Working with props

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import Post from "./components/Post";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <main>
+      <Post author="Max" body="React is complicated" />
+      <Post author="Phil" body="React is Full Course here" />
+    </main>
+  );
 }
 
 export default App;
