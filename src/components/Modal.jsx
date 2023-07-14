d6946bc37fc83d1a690c0d695ff7fc67c841aaa0
Polybe <55062877+Philippe35507@users.noreply.github.com>
2023-07-14 22:47:46 +0100

    object desctructuring: children + body + author

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,11 +1,11 @@
 import classes from "./Modal.module.css";
 
-function Modal(props) {
+function Modal({ children }) {
   return (
     <>
       <div className={classes.backdrop} />
       <dialog open className={classes.modal}>
-        {props.children}
+        {children}
       </dialog>
     </>
   );
