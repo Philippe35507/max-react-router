eefed435ffdfff96ab029374303479c150e6fc41
Polybe <55062877+Philippe35507@users.noreply.github.com>
2023-07-15 16:12:15 +0100

    Handling form submission  (console.log)

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,33 +1,17 @@
-import { useState } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
 import Modal from "./Modal";
 import classes from "./PostsList.module.css";
 
 function PostsList({ isPosting, onStopPosting }) {
-  const [enteredBody, setEnteredBody] = useState("");
-  const [enteredAuthor, setEnteredAuthor] = useState("");
-
-  function bodyChangeHandler(event) {
-    setEnteredBody(event.target.value);
-  }
-  function AuthorChangeHandler(event) {
-    setEnteredAuthor(event.target.value);
-  }
-
   return (
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
-          <NewPost
-            onBodyChange={bodyChangeHandler}
-            onAuthorChange={AuthorChangeHandler}
-            onCancel={onStopPosting}
-          />
+          <NewPost onCancel={onStopPosting} />
         </Modal>
       )}
       <ul className={classes.posts}>
-        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Phil" body="React's Full Course here" />
       </ul>
     </>
