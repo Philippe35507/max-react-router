5cce5e3cac0dae865af6c9a85a2972166b0ac56b
Polybe <55062877+Philippe35507@users.noreply.github.com>
2023-07-15 15:31:58 +0100

    useState modalIsVisible

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -5,8 +5,13 @@ import Modal from "./Modal";
 import classes from "./PostsList.module.css";
 
 function PostsList() {
+  const [modalIsVisible, setModalIsVisible] = useState(true);
   const [enteredBody, setEnteredBody] = useState("");
   const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function hideModalHandler() {
+    setModalIsVisible();
+  }
 
   function bodyChangeHandler(event) {
     setEnteredBody(event.target.value);
@@ -17,12 +22,14 @@ function PostsList() {
 
   return (
     <>
-      <Modal>
-        <NewPost
-          onBodyChange={bodyChangeHandler}
-          onAuthorChange={AuthorChangeHandler}
-        />
-      </Modal>
+      {modalIsVisible && (
+        <Modal onClose={hideModalHandler}>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={AuthorChangeHandler}
+          />
+        </Modal>
+      )}
       <ul className={classes.posts}>
         <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Phil" body="React's Full Course here" />
